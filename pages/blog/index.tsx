bfebc84ed375a458a5c694207f602331076899c4
Anthony Oleinik <antholeinik@gmail.com>
2021-12-19 10:13:25 -0600

    added counter

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -4,8 +4,8 @@ import path from 'path'
 import React from 'react'
 import { Layout } from '../../components/layout/layout'
 import { getBlogCategories, categoriesPath } from '../../helpers/get_posts'
-import { readFileSync } from 'fs'
-import { Button, Card } from 'react-bootstrap'
+import { readFileSync, readdirSync } from 'fs'
+import { Badge, Button, Card } from 'react-bootstrap'
 import { capitalizeWords } from '../../helpers/capitalize_words'
 import ReactMarkdown from 'react-markdown'
 
@@ -26,7 +26,14 @@ const Home = ({ categories }: InferGetStaticPropsType<typeof getStaticProps>) =>
                     return <>
                     <Card className="mb-3 d-inline-flex" key={v.name}>
                         <Card.Body>
-                            <Card.Title>{name}</Card.Title>
+                            <Card.Title className="d-flex justify-content-between">
+                                {name}
+                                <Badge bg="secondary">
+                                        <span className="font-weight-normal">
+                                            {v.size}
+                                        </span>
+                                </Badge>
+                            </Card.Title>
                             <Card.Text as="article">
                                 <ReactMarkdown>{v.summary}</ReactMarkdown>  
                             </Card.Text>
@@ -48,11 +55,13 @@ export const getStaticProps: GetStaticProps<{
     categories: {
         name: string;
         summary: string;
+        size: number;
     }[]
 }> = async () => {
     const categories = getBlogCategories()
         .map((category) => ({
             name: category,
+            size: readdirSync(path.join(categoriesPath, category)).length - 1,
             summary: readFileSync(
                 path.join(categoriesPath, category, 'summary.md')
             ).toString()
