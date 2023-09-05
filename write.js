415da9d1dff7123e0b5b49f121b1a236163653e2
oleina <oleina@google.com>
2023-09-05 15:02:50 -0700

    added some styling

--- write.js
+++ write.js
@@ -11,7 +11,10 @@ fs.readdirSync("./blog").forEach(file => {
 
     if (filetype == "md") {
         const md = fs.readFileSync(`./blog/${file}`, "utf-8")
-        fs.writeFileSync(`./out/${fileName}.html`, marked.parse(md))
+        const html = marked.parse(md)
+        const output = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="/index.css"><title>Anthony Oleinik - ${fileName}</title></head> <body>${html}</body></html>`
+
+        fs.writeFileSync(`./out/${fileName}.html`, output)
     } else {
         fs.copyFileSync(`./blog/${file}`, `./out/${file}`)
     }
