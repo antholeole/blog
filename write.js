03c90b3a00bd1ba189b7843cb028bc04918815fc
oleina <oleina@google.com>
2024-02-05 22:06:49 -0800

    a

--- write.js
+++ write.js
@@ -20,7 +20,6 @@ const fileToMd = (input) => {
         const html = marked.parse(md)
 
         const output = genOutput(fileName, html)
-        console.log(output)
 
         fs.writeFileSync(`${outPath}/${path}/${fileName}.html`, output)
     } else {
